c2f07f05f345aea3088bd7beb89dae3cf3db912c
diasd <ahmediarlyo@gmail.com>
2026-06-12 10:10:03 +0500

    Fix format_html usage in admin thumbnail methods to resolve TypeError

--- cars/admin.py
+++ cars/admin.py
@@ -6,7 +6,7 @@ from django.utils.html import format_html
 
 class CarAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
-        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.car_photo.url))
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />', object.car_photo.url)
 
     thumbnail.short_description = 'Car Image'
     list_display = ('id','thumbnail','car_title', 'brand', 'city', 'color', 'model', 'year', 'body_style', 'fuel_type', 'is_featured')
