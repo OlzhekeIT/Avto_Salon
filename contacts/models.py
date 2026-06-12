a4f31e9c39a8c6e330966d9909d945aad77500c0
diasd <ahmediarlyo@gmail.com>
2026-06-12 09:59:53 +0500

    Update Contact model to use Kazakhstan regions

--- contacts/models.py
+++ contacts/models.py
@@ -3,13 +3,36 @@ from datetime import datetime
 
 # Create your models here.
 class Contact(models.Model):
+    state_choice = (
+        ('ALA', 'Almaty'),
+        ('AST', 'Astana'),
+        ('SHY', 'Shymkent'),
+        ('AKM', 'Akmola Region'),
+        ('AKT', 'Aktobe Region'),
+        ('ALM', 'Almaty Region'),
+        ('ATY', 'Atyrau Region'),
+        ('VKO', 'East Kazakhstan Region'),
+        ('ZHA', 'Zhambyl Region'),
+        ('JET', 'Jetisu Region'),
+        ('ZKO', 'West Kazakhstan Region'),
+        ('KAR', 'Karaganda Region'),
+        ('KOS', 'Kostanay Region'),
+        ('KYZ', 'Kyzylorda Region'),
+        ('MAN', 'Mangystau Region'),
+        ('ABA', 'Abai Region'),
+        ('PAV', 'Pavlodar Region'),
+        ('SKO', 'North Kazakhstan Region'),
+        ('TUR', 'Turkistan Region'),
+        ('ULY', 'Ulytau Region'),
+    )
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     car_id = models.IntegerField()
     customer_need = models.CharField(max_length=100)
     car_title = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
+    state = models.CharField(choices=state_choice, max_length=100)
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=100)
     message = models.TextField(blank=True)
